c79176b1d109557042459b359528d0cbdf95dd81
Luke Schoen <ltfschoen@gmail.com>
2017-02-21 14:21:57 +1100

    Attempted to implement the Only Choice Technique

--- term01/lesson01/function2.py
+++ term01/lesson01/function2.py
@@ -1,6 +1,8 @@
 import sys; assert sys.version_info >= (3,6,0)
 from utils import display, boxes
 from utils import peers
+import functools
+import copy
 
 # Board
 unsolved_puzzle = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
@@ -44,40 +46,40 @@ def grid_values(grid):
             result[k] = '123456789'
     return result
 
-# def get_boxes_in_unit(box, boxes):
-#     """
-#     Input: Box i.e. B3
-#     Output: Array of all the boxes in the same unit/square as the given box (including the box itself)
-#     """
-#     unit1 = boxes[0:3] + boxes[9:12] + boxes[18:21]
-#     unit2 = boxes[3:6] + boxes[12:15] + boxes[21:24]
-#     unit3 = boxes[6:9] + boxes[15:18] + boxes[24:27]
-#     unit4 = boxes[27:30] + boxes[36:39] + boxes[45:48]
-#     unit5 = boxes[30:33] + boxes[39:42] + boxes[48:51]
-#     unit6 = boxes[33:36] + boxes[42:45] + boxes[51:54]
-#     unit7 = boxes[54:57] + boxes[63:66] + boxes[72:75]
-#     unit8 = boxes[57:60] + boxes[66:69] + boxes[75:78]
-#     unit9 = boxes[60:63] + boxes[69:72] + boxes[78:81]
-#
-#     if box in unit1:
-#         return unit1
-#     if box in unit2:
-#         return unit2
-#     if box in unit3:
-#         return unit3
-#     if box in unit4:
-#         return unit4
-#     if box in unit5:
-#         return unit5
-#     if box in unit6:
-#         return unit6
-#     if box in unit7:
-#         return unit7
-#     if box in unit8:
-#         return unit8
-#     if box in unit9:
-#         return unit9
-#
+def get_boxes_in_unit(box, boxes):
+    """
+    Input: Box i.e. B3
+    Output: Array of all the boxes in the same unit/square as the given box (including the box itself)
+    """
+    unit1 = boxes[0:3] + boxes[9:12] + boxes[18:21]
+    unit2 = boxes[3:6] + boxes[12:15] + boxes[21:24]
+    unit3 = boxes[6:9] + boxes[15:18] + boxes[24:27]
+    unit4 = boxes[27:30] + boxes[36:39] + boxes[45:48]
+    unit5 = boxes[30:33] + boxes[39:42] + boxes[48:51]
+    unit6 = boxes[33:36] + boxes[42:45] + boxes[51:54]
+    unit7 = boxes[54:57] + boxes[63:66] + boxes[72:75]
+    unit8 = boxes[57:60] + boxes[66:69] + boxes[75:78]
+    unit9 = boxes[60:63] + boxes[69:72] + boxes[78:81]
+
+    if box in unit1:
+        return unit1
+    if box in unit2:
+        return unit2
+    if box in unit3:
+        return unit3
+    if box in unit4:
+        return unit4
+    if box in unit5:
+        return unit5
+    if box in unit6:
+        return unit6
+    if box in unit7:
+        return unit7
+    if box in unit8:
+        return unit8
+    if box in unit9:
+        return unit9
+
 # def get_peers_for_box(box, boxes):
 #     """
 #     Input: Box i.e. B3
@@ -141,14 +143,102 @@ def eliminate(values):
             digit = update_dict[k]
             for pk in peer_keys:
                 update_dict[pk] = update_dict[pk].replace(digit,'')
-    return (update_dict)
+    return update_dict
 
+def only_choice(values):
+    """
+    Finalize all values that are the only choice for a unit.
+
+    Go through all the units/squares, and whenever there is a unit with
+    a box that contains an unsolved value that only fits in that one box,
+    assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in Only Choices.
+    """
+
+    # # Course Solution:
+    # for unit in unitlist:
+    #     for digit in '123456789':
+    #         dplaces = [box for box in unit if digit in values[box]]
+    #         if len(dplaces) == 1:
+    #             values[dplaces[0]] = digit
+    # return values
+
+    # My Solution (Marked Incorrect):
+
+    # print("values: ", values)
+
+    latest_dict = copy.deepcopy(values)
+    temp_dict = copy.deepcopy(values)
+    update_dict = values
+    for box, val in update_dict.items():
+        # SQUARE BOX - get all boxes that are within the same unit/square as the given box
+        # print("box: ", box)
+        boxes_in_same_square_as_box = get_boxes_in_unit(box, boxes)
+        # print("boxes in same square: ", boxes_in_same_square_as_box)
+
+        # create deep copy of original dict and remove any boxes that are not in square of current box
+        copy_update_dict = copy.deepcopy(update_dict)
+        for k in update_dict:
+            if not k in boxes_in_same_square_as_box:
+                del copy_update_dict[k]
+        # print(copy_update_dict)
+
+        # create deep copy of dict containing all boxes in square and remove any boxes with only 1 value
+        square_dict = copy.deepcopy(copy_update_dict)
+        for k in copy_update_dict:
+            if len(copy_update_dict[k]) == 1:
+                del square_dict[k]
+        # print("square_dict with unsolved boxes: ", square_dict)
+
+        if len(square_dict) != 0:
+            # create list of all the values (including duplicates) from unsolved boxes in current square
+            unsolved_boxes_keys = []
+            unsolved_boxes_values = []
+            for k, v in square_dict.items():
+                unsolved_boxes_keys.append(k)
+                unsolved_boxes_values.append(v)
+            # print(unsolved_boxes_keys)
+            # print(unsolved_boxes_values)
+            all_unsolved_boxes_values = functools.reduce(lambda x,y: x+y, unsolved_boxes_values)
+            # print("all_unsolved_boxes_values: ", all_unsolved_boxes_values)
+
+            list_unresolved = list(all_unsolved_boxes_values)
+            uniq = set(list_unresolved)
+            # print("uniq: ", uniq)
+            only_ones = [] # there may be more than one box in a square having a value that only occurs
+            # once in unsolved boxes of that square
+            for u in uniq:
+                if all_unsolved_boxes_values.count(u) == 1:
+                    only_ones.append(u)
+            # print("only ones: ", only_ones)
+
+            # update relevant boxes in temp_dict to be returned (overwrite the remaining possibilities with
+            # the only one for that box
+            temp_dict = latest_dict
+            for k, v in latest_dict.items():
+                if k in unsolved_boxes_keys:
+                    for o in only_ones:
+                        if o in v:
+                            # print("only one: ", o)
+                            # print("v is: ", v)
+                            index_of_only_one = only_ones.index(o)
+                            # print("index of only one: ", index_of_only_one)
+                            temp_dict[k] = only_ones[index_of_only_one]
+            latest_dict = temp_dict
+            # print("updated latest_dict: ", latest_dict)
+
+    print(latest_dict)
+    return latest_dict
 
 # Visualisation of Sudoku puzzle generated in dictionary form
 result = grid_values(unsolved_puzzle)
 vis = display(result)
 output = eliminate(result)
 print(output)
+output2 = only_choice(output)
+
 """ Sample Output:
   45   4578   3   |  9     2     17  |  6    5789   57
   9   24678   47  |  3     47    5   |  78   278    1
