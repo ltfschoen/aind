aed82993bb2a36dd55b91edb819152a65dbc7b88
Luke Schoen <ltfschoen@gmail.com>
2017-02-24 09:31:18 +1100

    Added debugging logs

--- term01/lesson01/function2.py
+++ term01/lesson01/function2.py
@@ -162,7 +162,8 @@ def only_choice(values):
     # Course Solution:
     for unit in unitlist:
         for digit in '123456789':
-            dplaces = [box for box in unit if digit in values[box]]
+            dplaces = [box for box in unit
+                       if digit in values[box]]
             if len(dplaces) == 1:
                 values[dplaces[0]] = digit
     return values
@@ -247,18 +248,21 @@ def reduce_puzzle(values):
     stalled = False
     while not stalled:
         # Check how many boxes have a determined value
-        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        solved_values_before = len([box for box in values.keys()
+                                    if len(values[box]) == 1])
 
         # Your code here: Use the Eliminate Strategy
         values = eliminate(values)
         # Your code here: Use the Only Choice Strategy
         values = only_choice(values)
         # Check how many boxes have a determined value, to compare
-        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        solved_values_after = len([box for box in values.keys()
+                                   if len(values[box]) == 1])
         # If no new values were added, stop the loop.
         stalled = solved_values_before == solved_values_after
         # Sanity check, return False if there is a box with zero available values:
-        if len([box for box in values.keys() if len(values[box]) == 0]):
+        if len([box for box in values.keys()
+                if len(values[box]) == 0]):
             return False
     return values
 
@@ -291,13 +295,15 @@ def search(values):
 def run(puzzle_with_difficulty):
     # Visualisation of Sudoku puzzle generated in dictionary form
     result = grid_values(puzzle_with_difficulty)
-    vis = display(result)
+    print("result: ", result)
     output = eliminate(result)
     print(output)
     # output2 = only_choice(output)
     # output2 = reduce_puzzle(output)
     output2 = search(output)
-    print(output2)
+    print("output2: ", output2)
+    if not isinstance(output2, bool):
+        display(output2)
 
 run(easy_unsolved_puzzle)
 run(hard_unsolved_puzzle)
