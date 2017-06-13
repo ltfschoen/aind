2fd2c7332e8ac86611f92d95679a376a5b7b9359
Luke Schoen <ltfschoen@gmail.com>
2017-06-13 11:25:23 +1000

    docs: Add comments to NN

--- term02/lesson02/part30_keras_neural_network_xor/neural_network.py
+++ term02/lesson02/part30_keras_neural_network_xor/neural_network.py
@@ -10,32 +10,52 @@ np.random.seed(42)
 X = np.array([[0,0],[0,1],[1,0],[1,1]]).astype('float32')
 y = np.array([[0],[1],[1],[0]]).astype('float32')
 
-# Initial Setup for Keras
+# Setup Keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten
 
 # One-hot encoding the output
 y = np_utils.to_categorical(y)
 
-# Building the model
+# Create Sequential Model with Sequential() wrapper for neural network model
+# to provide common functions like fit(), evaluate(), compile()
 xor = Sequential()
-# Hint: This next line is where you can change the size of the first hidden layer, it's current size is 8
+
+# Add Keras Layers (neural network layers including fully connected,
+# max pool, and activation layers) using function add()
+
+# 1st Layer - Add NN Layer with input data of size: 8 and nodes (dimensions): 2
+#   Note: Only required to set input dimensions for first layer since Keras infers shape of remainder
+#   Hint: This next line is where you can change the size of the first hidden layer, it's current size is 8
 xor.add(Dense(8, input_dim=2))
-# Hint: This next line is where you can change the activation funcion to "relu"
-xor.add(Activation("sigmoid"))
-xor.add(Dense(2))
+
+# 2nd Layer - Add sigmoid activation layer
+#   Hint: This next line is where you can change the activation function to "relu"
 xor.add(Activation("sigmoid"))
 
+# 3rd Layer - Add fully connected layer with 2 nodes (dimensions)
+#   Note: 3rd layer (last layer output of model) takes output of 1st/2nd Layer
+#         and sets output dimensions
+xor.add(Dense(2))
+
+# 4th Layer - Add sigmoid activation layer
+xor.add(Activation("sigmoid"))
+
+# Compile the previously built model
+#   - Loss Function             - categorical_crossentropy
+#   - Optimiser                 - adam
+#   - Metrics (to evaluate)     - accuracy
 xor.compile(loss="categorical_crossentropy", optimizer="adam", metrics = ['accuracy'])
 
-# Uncomment this line to print the model architecture
+# Print the model architecture
 # xor.summary()
 
-# Fitting the model
-# Hint: This next line is where you can change the number of epochs, it's set to 10 now.
+# Fit the model
+#   - Epoch (Note: Keras 1 nb_epoch; Keras 2 epochs)
+#   - Verbose
 history = xor.fit(X, y, nb_epoch=10, verbose=0)
 
-# Scoring the model
+# Scoring and evaluate the model
 score = xor.evaluate(X, y)
 print("\nAccuracy: ", score[-1])
 
